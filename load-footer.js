01be5c8105a1db324e8bc04de42ab2fb94ae63dc
Brumous <nippusidee@gmail.com>
2026-03-26 18:24:34 +0200

    Update load-footer.js

--- load-footer.js
+++ load-footer.js
@@ -1,7 +1,7 @@
 document.getElementById("footer").innerHTML = `
     <footer class="site-footer>
         <p>Brumous Studios © 2026</p>
-        <div> class="footer-links"</div>
+        <div class="footer-links">
             <a href="#">Youtube</a>  |  
             <a href="#">Twitter / X</a>
         </div>
