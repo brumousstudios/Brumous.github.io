243c204987c39460de3e0f35324e41c9534348fd
Brumous <nippusidee@gmail.com>
2026-03-26 18:23:10 +0200

    Add files via upload

---
+++ load-footer.js
@@ -0,0 +1,9 @@
+document.getElementById("footer").innerHTML = `
+    <footer class="site-footer>
+        <p>Brumous Studios © 2026</p>
+        <div> class="footer-links"</div>
+            <a href="#">Youtube</a>  |  
+            <a href="#">Twitter / X</a>
+        </div>
+    </footer>
+`;
